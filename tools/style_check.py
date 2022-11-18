434e0e103ccc089645bc8858f4362e1bba771abb
Vu Anh <anhv.ict91@gmail.com>
2022-11-18 18:42:53 +0000

    update

--- tools/style_check.py
+++ tools/style_check.py
@@ -1,12 +1,23 @@
 """ Document Style Check
 """
+import sys
 from os.path import dirname, join
 
 
 def check_content(content):
-    return True
+    # Must have line "From wikipedia"
+    print("Rule: Must have line \"From wikipedia\"")
+    rule_valid = False
+    for line in content.splitlines():
+        print(line)
+        if line.strip() == "From wikipedia":
+            rule_valid = True
+    if not rule_valid:
+        raise Exception("Style Check Error")
+        sys.exit(-1)
 
-if __name__ == '__main__':    
+
+if __name__ == '__main__':  
     # load templates
     PROJECT_FOLDER = dirname(dirname(__file__))
     template_file = join(PROJECT_FOLDER, "tools", "template.md")
