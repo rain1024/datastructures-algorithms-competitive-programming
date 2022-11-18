be2c31cd8ac87ef9a3bf67bc881cad08a8e9a169
Vu Anh <anhv.ict91@gmail.com>
2022-11-18 18:56:51 +0000

    update

--- tools/style_check.py
+++ tools/style_check.py
@@ -2,26 +2,28 @@
 """
 import sys
 from os.path import dirname, join
+import yaml
+import re
 
 
-def check_content(content):
-    # Must have line "From wikipedia"
-    print("Rule: Must have line \"From wikipedia\"")
-    rule_valid = False
-    for line in content.splitlines():
-        print(line)
-        if line.strip() == "From wikipedia":
-            rule_valid = True
-    if not rule_valid:
-        raise Exception("Style Check Error")
-        sys.exit(-1)
+def check_content(file, rules):
+    with open(file, encoding="utf-8") as f:
+        content = f.read(f)
 
+    for rule in rules:
+        search = rule['search']
+        result = re.search(search, content)
+        if not result:
+            message = f"[❌] {rule['name']}\n{rule}"
+            raise Exception(message)
 
-if __name__ == '__main__':  
-    # load templates
+if __name__ == '__main__':
     PROJECT_FOLDER = dirname(dirname(__file__))
     template_file = join(PROJECT_FOLDER, "tools", "template.md")
+    rules_file = join(PROJECT_FOLDER, "tools", "rules.yaml")
 
-    with open(template_file, encoding="utf-8") as f:
-        content = f.read()
-    check_content(content)
+    with open(rules_file, 'r', encoding='utf-8') as f:
+        rules = yaml.safe_load(f)
+
+    check_content(template_file, rules)
+    print("✅ Check template file")
