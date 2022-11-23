c78944a0e324482c43c9e2c119858c77a027cd49
Vu Anh <anhv.ict91@gmail.com>
2022-11-24 04:18:59 +0700

    add what should i do

---
+++ tools/ai/how_should_i_do.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+problems = pd.read_csv(
+  "/Users/anhv/projects/datastructures-algorithms-competitive-programming/problems/problems.csv",
+  delimiter=";",
+  header=0)
+
+practice = pd.read_csv(
+  "/Users/anhv/projects/datastructures-algorithms-competitive-programming/problems/anhv_practice.csv",
+  delimiter=";",
+  header=0)
+success = practice[practice['result'] == 'success']
+success_problems_id = set(success["problem_id"])
+
+available_problems = problems[~problems["id"].isin(success_problems_id)]
+
+n_problems = len(available_problems)
+rating = 1400
+print(f" ====================================================================")
+print(f"|                          VISUAL ASSISTANT                          |")
+print(f" ====================================================================")
+print(f">>> Hello. I'm Virtual Assistant to help you go to the next level.")
+print(f">>> ... ")
+print("\n" * 2)
+print(f">>> First thing first. Let's known your self.")
+print(f">>> Your rating is {rating}")
+print(f">>> ...")
+print("\n" * 2)
+print(f">>> I have a good news. There are {n_problems} problems for you")
+print(f">>> Can you code something, try the following problem:")
+print(available_problems.sample(1))
+print(f">>> ...\n" * 5)
+print(f">>> Or <{n_problems}> is not big enough. Try to find others.")
+print(f">>> https://codeforces.com/problemset")
+print(f">>> ...\n" * 3)
+print(f">>> Good luck, my friend.")
+
