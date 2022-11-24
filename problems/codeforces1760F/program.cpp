52779ca93d72e4f3db255a9ab67860145224e6ba
Vu Anh <anhv.ict91@gmail.com>
2022-11-24 20:11:44 +0700

    update

---
+++ problems/codeforces1760F/program.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  long long c, d;
+  int n;
+  while(tt--){
+    cin >> n >> c >> d;
+    vector<int> a;
+    int ai;
+    for(int i=0; i<n; i++){
+      cin >> ai;
+      a.push_back(ai);
+    }
+    cout << 0 << '\n';
+  }
+  return 0;
+}
