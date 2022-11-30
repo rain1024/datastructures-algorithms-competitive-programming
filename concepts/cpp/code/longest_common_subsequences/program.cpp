10234acfe70e1c3d57b7a17c031e828e8594467c
Vu Anh <anhv.ict91@gmail.com>
2022-11-30 22:41:10 +0700

    update

---
+++ concepts/cpp/code/longest_common_subsequences/program.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+
+int lcs(string a, string b) {
+  int n = a.size();
+  int m = b.size();
+  int dp[n + 1][m + 1];
+  dp[0][0] = 0;
+  for (int i = 0; i <= n; i++) {
+    dp[i][0] = 0;
+  }
+  for (int j = 0; j <= m; j++) {
+    dp[0][j] = 0;
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (a[i] == a[j]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[n][m];
+}
+
+int main() {
+  string a, b;
+  
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    cin >> a >> b;
+    cout << lcs(a, b) << endl;
+  }
+}
