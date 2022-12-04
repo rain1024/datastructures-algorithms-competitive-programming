c1c0827940a5255de926c96a729e8bb21c841179
Vu Anh <anhv.ict91@gmail.com>
2022-12-04 23:31:48 +0700

    GH-44: solve codeforces 1A

--- problems/codeforces1A/solution.cpp
+++ problems/codeforces1A/solution.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 #include <cmath>
-#include <vector>
  
 using namespace std;
 
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int tt;
-  cin >> tt;
-  while(tt--){
-    cout << '\n';
-  }
+  long long int n, m, a, k;
+  cin >> n >> m >> a;
+  k = ceil(n * 1.0 / a) * ceil(m * 1.0 / a);
+  cout << k << endl;
   return 0;
 }
