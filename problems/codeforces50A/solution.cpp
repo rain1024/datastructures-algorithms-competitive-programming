d1eaaa2a4da3ef67b08c972c1ee2938792bdeb2c
Vu Anh <anhv.ict91@gmail.com>
2022-12-06 20:10:47 +0700

    update

---
+++ problems/codeforces50A/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <algorithm>
+ 
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int M, N;
+  cin >> M >> N;
+  cout << (M * N) / 2 << endl;
+  return 0;
+}
