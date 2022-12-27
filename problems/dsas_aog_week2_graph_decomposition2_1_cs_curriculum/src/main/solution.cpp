e8c08c7f75e5f0b90988925515d219b4bf4051a5
Vu Anh <anhv.ict91@gmail.com>
2022-12-27 07:52:09 +0700

    update

---
+++ problems/dsas_aog_week2_graph_decomposition2_1_cs_curriculum/src/main/solution.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--){
+    cout << tt << '\n';
+  }
+  return 0;
+}
