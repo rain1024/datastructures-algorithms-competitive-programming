2e19e1e8610489d0af3e03a030f9025d5ef1971c
Vu Anh <anhv.ict91@gmail.com>
2022-12-27 12:48:39 +0700

    update

--- problems/dsas_aog_week2_graph_decomposition2_1_cs_curriculum/src/main/acyclicity.cpp
+++ problems/dsas_aog_week2_graph_decomposition2_1_cs_curriculum/src/main/acyclicity.cpp
@@ -1,22 +1,53 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
 using std::pair;
+using std::vector;
 
-int acyclic(vector<vector<int> > &adj) {
-  //write your code here
+#define N 1000
+bool visited[N];
+bool recursion_stack[N];
+
+bool dfs(vector<vector<int>> adj, int i){
+  visited[i] = true;
+  recursion_stack[i] = true;
+  for(auto j: adj[i]){
+    if(!visited[j]){
+      bool output = dfs(adj, j);
+      if(output){
+        return true;
+      }
+    } else {
+      if(recursion_stack[j]){
+        return true;
+      }
+    }
+  }
+  recursion_stack[i] = false;
+  return false;
+};
+
+// return true if the give graph has cycle.
+int acyclic(vector<vector<int>> &adj, int n) {
+  for(int i=0; i<n; i++){
+    if(!visited[i]){
+      bool output = dfs(adj, i);
+      if(output){
+        return 1;
+      }
+    }
+  }
   return 0;
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<vector<int>> adj(n, vector<int>());
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
   }
-  std::cout << acyclic(adj);
+  std::cout << acyclic(adj, n);
 }
