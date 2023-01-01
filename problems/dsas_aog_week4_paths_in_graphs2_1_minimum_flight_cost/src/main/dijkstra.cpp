65f6ec2e5d4f41ba50614a81323eaf4e5c64b1a4
Vu Anh <anhv.ict91@gmail.com>
2023-01-01 09:22:26 +0700

    update

--- problems/dsas_aog_week4_paths_in_graphs2_1_minimum_flight_cost/src/main/dijkstra.cpp
+++ problems/dsas_aog_week4_paths_in_graphs2_1_minimum_flight_cost/src/main/dijkstra.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <limits>
 
 using std::vector;
 using std::queue;
 using std::pair;
 using std::priority_queue;
 
+using namespace std;
+int n, m;
+
 long long distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
   //write your code her
-  return -1;
+  long long MAX_DISTANCE = numeric_limits<long long>::max();
+  vector<long long> dist(n, MAX_DISTANCE);
+  vector<int> visited(n, 0);
+
+  auto cmp = [](pair<int, long long> a, pair<int, long long> b){
+    return a.second > b.second;
+  };
+  priority_queue<pair<int, long long>, vector<pair<int, long long>>, decltype(cmp)> pq(cmp);
+
+  pq.emplace(s, 0);
+  dist[s] = 0;
+  visited[s] = 1;
+
+  while(!pq.empty()){
+    int u = pq.top().first;
+    pq.pop();
+    visited[u] = 1;
+
+    // update the distance of adjacent vertices
+    for(int i=0; i<adj[u].size(); i++){
+      int v = adj[u][i];
+      long long w = cost[u][i];
+
+      if(dist[v] > dist[u] + w){
+        dist[v] = dist[u] + w;
+        if(!visited[v]){
+          pq.emplace(v, dist[v]);
+        }
+      }
+    }
+  }
+  return (dist[t] == MAX_DISTANCE) ? -1 : dist[t];
 }
 
 int main() {
-  int n, m;
   std::cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
   vector<vector<int> > cost(n, vector<int>());
