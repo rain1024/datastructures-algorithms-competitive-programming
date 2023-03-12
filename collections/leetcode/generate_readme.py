f413b2c5ea5cbb8b014af8e3817a8bc5fe47be5c
Vu Anh <anhv.ict91@gmail.com>
2023-03-12 23:59:36 +0700

    update

--- collections/leetcode/generate_readme.py
+++ collections/leetcode/generate_readme.py
@@ -16,7 +16,7 @@ print(solve_problems)
 
 table = ""
 table = "<table>\n<tr>"
-for i in range(1, 3000):
+for i in range(1, 2589+1):
     if i % 10 == 1 and i > 1:
         table += "<tr>\n"
     table += f"<td>{i}</td>\n"
