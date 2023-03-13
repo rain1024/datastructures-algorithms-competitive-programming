1e2fc46470da28fed6033d1cd1f72fd4a17de2b2
Vu Anh <anhv.ict91@gmail.com>
2023-03-13 14:12:53 +0700

    update

--- tools/make_docs.py
+++ tools/make_docs.py
@@ -1,19 +1,30 @@
 import os
 from os.path import join, dirname, abspath
+
 # read concepts_data.yaml
 
 import yaml
 
 cwd = dirname(abspath(__file__))
 workspace_dir = dirname(cwd)
-readme_dir = join(workspace_dir, 'readme')
+readme_dir = join(workspace_dir, "readme")
 
-data = yaml.safe_load(open(join(cwd, 'concepts_data.yaml')))
+data = yaml.safe_load(open(join(cwd, "concepts_data.yaml")))
 print(data)
 
 # write file in readme_dir
-programming_languages = ['abstract', 'python']
+programming_languages = [
+    "abstract",
+    "javascript",
+    "python",
+    "java",
+    "nodejs",
+    "typescript",
+    "csharp",
+    "cpp",
+    "php",
+]
 for programing_language in programming_languages:
-    with open(join(readme_dir, programing_language, 'README.md'), 'w') as f:
-        content = 'hi hi'
+    with open(join(readme_dir, programing_language, "README.md"), "w") as f:
+        content = "hi hi"
         f.write(content)
