be5d72871fa01979de89f49175ea7aef032d6049
Vu Anh <anhv.ict91@gmail.com>
2023-03-14 15:38:50 +0700

    GH-125: Add Codeforces collection (#186)

---
+++ collections/codeforces/README.template.md
@@ -0,0 +1,16 @@
+# Codeforces
+
+**Codeforces Solutions**
+
+* 🟢 Easy
+* 🟡 Medium
+* 🔴 Hard 
+* 👑 Master
+
+**Problems**
+
+<!-- table -->
+
+## Similar Repositories
+
+* https://github.com/seikhchilli/codeforces-solution
---
+++ collections/codeforces/generate_readme.py
@@ -0,0 +1,57 @@
+import yaml
+import os
+from os.path import join
+
+wd = os.path.dirname(os.path.realpath(__file__))
+# read template file
+with open(join(wd, 'README.template.md'), 'r') as f:
+    tempalte = f.read()
+
+# read data.yaml
+with open(join(wd, 'data.yaml'), 'r') as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+
+problems = {}
+solved_problems = set()
+for item in data['problems']:
+    id = item['name']
+    problems[id] = item
+    solved_problems.add(id)
+print(f"Solve {len(solved_problems)} problems")
+
+def get_level(problem):
+    if "score" not in problem:
+        return ""
+    score = problem["score"]
+    if score < 1200:
+        return "🟢&nbsp"
+    elif score < 1600:
+        return "🟡&nbsp"
+    elif score < 1800:
+        return "🔴&nbsp"
+    else:
+        return "👑&nbsp"
+
+table = ""
+table = "<table>\n<tr>"
+for i in range(1, 1804):
+    table += "<tr>\n"
+    tr = "<tr>\n"
+    for j in ['A', 'B', 'C', 'D', 'E', 'F', 'G']:
+        id = f"{i}{j}"
+        if id not in solved_problems:
+            tr += f"<td>{id}</td>\n"
+        else:
+            td = "<td>"
+            td += get_level(problems[id])
+            td += f"<a href='https://github.com/rain1024/datastructures-algorithms-competitive-programming/tree/main/problems/codeforces{id}'>{id}</a>"
+            td += "</td>\n"
+            tr += td
+    tr += "</tr>\n"
+    table += tr
+    
+table += "</table>"
+
+with open(join(wd, 'README.md'), 'w') as f:
+    content = tempalte.replace('<!-- table -->', table)
+    f.write(content)
