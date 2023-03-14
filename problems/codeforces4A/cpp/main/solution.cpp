566608bdef0b1b1cc4cfdb5bfd670211e0f83f21
Vu Anh <anhv.ict91@gmail.com>
2023-03-14 23:01:24 +0700

    GH-169: Solve codeforces 4A (#190)

---
+++ problems/codeforces4A/cpp/main/solution.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+// #include <fstream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  // filesystem::path filepath =
+  //     filesystem::current_path().parent_path().parent_path() / "data" /
+  //     "1.in";
+  // ifstream file(filepath);
+  //   cin.rdbuf(file.rdbuf());  // redirect cin to file
+  int num;
+  cin >> num;
+  if (num % 2 == 0 && num > 2) {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
+  return 0;
+}
