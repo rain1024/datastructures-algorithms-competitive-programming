4313a62ce001861766f6d148489b00bdc2a7f4ba
Quang Pham <Quangphampm@gmail.com>
2023-04-02 23:38:11 +0700

    GH-169: Solve Leetcode 67, 69 (#280)

--- tools/craft/craft.py
+++ tools/craft/craft.py
@@ -1,11 +1,12 @@
 import os
-from os.path import dirname, join
-import sys
 import shutil
+import sys
+from os.path import dirname, join
 
 CURRENT_DIR = dirname(__file__)
 PROBLEMS_DIR = join(dirname(dirname(CURRENT_DIR)), "problems")
 print(PROBLEMS_DIR)
+SUPPORTED_LANGUAGE = ['cpp','python']
 
 
 def replace_string_in_file(filename, old, new):
@@ -17,23 +18,27 @@ def replace_string_in_file(filename, old, new):
 
 
 class CodeGenerator:
-    def __init__(self, domain, problem_id):
+    def __init__(self, domain, problem_id, language):
         self.domain = domain
         self.problem_id = problem_id
+        self.language = language
 
     def generate(self):
         domain = self.domain
         problem_id = self.problem_id
         problem_dir = join(PROBLEMS_DIR, f"{domain}{problem_id}")
         try:
-            shutil.copytree(join(CURRENT_DIR, "templates", domain), problem_dir)
+            if self.language not in SUPPORTED_LANGUAGE:
+                raise Exception(f'{language} is not yet supported.')
+            exclude_folders = [folder for folder in SUPPORTED_LANGUAGE if folder != language]
+            shutil.copytree(join(CURRENT_DIR, "templates", domain), problem_dir, ignore=shutil.ignore_patterns(*exclude_folders))
         except Exception as e:
             print(e)
             sys.exit(1)
 
 class CodeforcesGenerator(CodeGenerator):
-    def __init__(self, domain, problem_id):
-        super().__init__(domain, problem_id)
+    def __init__(self, domain, problem_id, language):
+        super().__init__(domain, problem_id, language)
 
     def generate(self):
         super().generate()
@@ -60,19 +65,21 @@ class CodeforcesGenerator(CodeGenerator):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Boilderplate to generate workspace for solving problem.\n")
-        print("Usage: python crape.py domain problem_id\n\n")
+        print("Usage: python craft.py domain problem_id language\n\n")
         print("Examples:\n")
         print("\tpython craft.py codeforces 100A")
         print("\tpython craft.py leetcode 217")
+        print("\tpython craft.py leetcode 217 python")
         print("\tpython craft.py aoc 2022day2")
         print("\tpython craft.py codejam 2022PunchedCards")
         print("\tpython craft.py dsas _aog_week1_graph_decomposition1_1_finding_exit_from_maze")
         sys.exit(1)
     domain = sys.argv[1]
     problem_id = sys.argv[2]
+    language = sys.argv[3] if len(sys.argv) == 4 else 'cpp'
     if domain == "codeforces":
         GeneratorClass = CodeforcesGenerator
     else:
         GeneratorClass = CodeGenerator
-    generator = GeneratorClass(domain, problem_id)
+    generator = GeneratorClass(domain, problem_id, language)
     generator.generate()
